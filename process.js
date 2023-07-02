d204df12ccdb3387594237a21a59cfa081ebca94
Anthony O <antman@ANTMANS-Air.lan>
2023-07-02 14:41:47 -0400

    still working

---
+++ process.js
@@ -0,0 +1,43 @@
+// const title = document.querySelector("h1");
+
+// const Div = document.querySelector("div")
+
+  
+
+// const newItemForlist = document.createElement("p");
+//   newItemForlist.innerText = Event.target("item-name").value
+// console.log("new ptag:" , newItemForlist)
+
+// newItemForlist.addEventListener("mouseover", (event)=> (
+//     console.log(event.target.innerText)
+// ))
+
+//   groceryListDiv.append(newItemForlist)
+
+
+  
+  
+//   const newItemForlist2 = document.createElement("p")
+//   newItemForlist2.innerText= "cucumbers"
+//   console.log("new ptag" ,newItemForlist2)
+ 
+//   groceryListDiv.append(newItemForlist2)
+
+  
+  
+  
+// const groceryFormForNewItem = document.querySelector("form")
+//   groceryFormForNewItem.addEventListener("submit", (event)=>{
+//     console.log("submitting form !: ", event.target["item-weight"].value)
+
+//     event.preventDefault()
+
+//   })
+
+//   const newItemForlist3 = document.createElement("p")
+//   newItemForlist2.innerText= event.target("item-name").value
+  
+
+//   groceryListDiv.append(newItemForlist3)
+
+//   groceryFormForNewItem.reset()
