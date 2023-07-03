69aeb47145e8b3042c9fae27ebeb6a67acfc0454
Anthony O <antman@ANTMANS-Air.lan>
2023-07-02 20:33:51 -0400

    trying to get cart to display

--- process.js
+++ process.js
@@ -1,43 +1,32 @@
-// const title = document.querySelector("h1");
+const title = document.querySelector("h1");
 
-// const Div = document.querySelector("div")
+const div = document.querySelector("div")
+
+let incart = false
+
+
+const h2ForAddToCart = document.querySelector("h2")
+h2ForAddToCart.addEventListener("click" , (event)=>{
+incart = !incart
+  console.log("ADDED TO CART", event.target.innerText)
+})
+
+
+const h3ForAddToCart = document.querySelector("h3")
+h3ForAddToCart.addEventListener("click" , (event)=>{
+  incart = !incart
+  console.log("ADDED TO CART", event.target.innerText)
+})
+
+
+const FormForNewItem = document.querySelector("button")
+  FormForNewItem.addEventListener("submit", (event)=>{
+   
+    console.log("submitting form !: ", event.target[0].value)
+
+    event.preventDefault()
+   
+  })
 
   
-
-// const newItemForlist = document.createElement("p");
-//   newItemForlist.innerText = Event.target("item-name").value
-// console.log("new ptag:" , newItemForlist)
-
-// newItemForlist.addEventListener("mouseover", (event)=> (
-//     console.log(event.target.innerText)
-// ))
-
-//   groceryListDiv.append(newItemForlist)
-
-
-  
-  
-//   const newItemForlist2 = document.createElement("p")
-//   newItemForlist2.innerText= "cucumbers"
-//   console.log("new ptag" ,newItemForlist2)
  
-//   groceryListDiv.append(newItemForlist2)
-
-  
-  
-  
-// const groceryFormForNewItem = document.querySelector("form")
-//   groceryFormForNewItem.addEventListener("submit", (event)=>{
-//     console.log("submitting form !: ", event.target["item-weight"].value)
-
-//     event.preventDefault()
-
-//   })
-
-//   const newItemForlist3 = document.createElement("p")
-//   newItemForlist2.innerText= event.target("item-name").value
-  
-
-//   groceryListDiv.append(newItemForlist3)
-
-//   groceryFormForNewItem.reset()
